10006c2eda7f65b1c1b0b35106576ad4b948ae59
Shkirenkoroma <61347452+Shkirenkoroma@users.noreply.github.com>
2022-09-16 13:26:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper-container', {
+  
+   
+   loop: true,
+ 
+navigation: {
+     nextEl: '.slider-button-next',
+     prevEl: '.slider-button-prev',
+   },
+ 
+
+   scrollbar: {
+     el: '.swiper-scrollbar',
+   },
+ });
